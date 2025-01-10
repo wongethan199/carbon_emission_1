1b4ae227952fb1a4d71f51ccd52dd98c43ba2c08
wongethan199 <wongethan199@gmail.com>
2025-01-10 12:00:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 #run first and make sure the files are in the colab first
 from setuptools import setup, find_packages
-from streamlit import st
+import streamlit as st
 from os import path
 here = path.abspath(path.dirname(__file__))
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Education',
-    'Intended Audience :: Customer Service',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Information Technology',
-    'Intended Audience :: Science/Research',
-    'Intended Audience :: Telecommunications Industry',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 3'
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Intended Audience :: Customer Service',
+  'Intended Audience :: Developers',
+  'Intended Audience :: Information Technology',
+  'Intended Audience :: Science/Research',
+  'Intended Audience :: Telecommunications Industry',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: Apache Software License',
+  'Programming Language :: Python :: 3'
 ]
 
 setup(
-    name='searoute',
-    version='1.4.2',
-    description='A python package for generating the shortest sea route between two points on Earth.',
-    #long_description=open('README.md').read() + '\n\n' +
-    #open('CHANGELOG.txt').read(),
-    long_description_content_type="text/markdown",
-    url='',
-    author='Gent Halili',
-    author_email='genthalili@users.noreply.github.com',
-    license='Apache License 2.0',
-    classifiers=classifiers,
-    keywords='searoute map sea route ocean ports',
-    packages=find_packages(),
-    install_requires=['geojson', 'networkx'],
-    project_urls={
-        "Documentation": "https://github.com/genthalili/searoute-py/blob/main/README.md",
-        "Source": "https://github.com/genthalili/searoute-py",
-    },
-    include_package_data=True,
+  name='searoute',
+  version='1.4.2',
+  description='A python package for generating the shortest sea route between two points on Earth.',
+  #long_description=open('README.md').read() + '\n\n' +
+  #open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
+  url='',
+  author='Gent Halili',
+  author_email='genthalili@users.noreply.github.com',
+  license='Apache License 2.0',
+  classifiers=classifiers,
+  keywords='searoute map sea route ocean ports',
+  packages=find_packages(),
+  install_requires=['geojson', 'networkx'],
+  project_urls={
+      "Documentation": "https://github.com/genthalili/searoute-py/blob/main/README.md",
+      "Source": "https://github.com/genthalili/searoute-py",
+  },
+  include_package_data=True,
 )
 import searoute as sr
 import pandas
